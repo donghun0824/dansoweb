5e355518f1461ff318ffc857081f7a3d75dbd332
정동훈 <donghun@jeongdonghun-ui-MacBookAir.local>
2025-12-15 10:59:07 +0900

    Fix: Add eventlet monkey_patch and requirements

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import eventlet 
+eventlet.monkey_patch()
 from flask import Flask, render_template, jsonify, request, send_from_directory, redirect, url_for, session
 from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
 from flask_socketio import SocketIO, emit
